d16e847e9b74323842b7460c43ef7f396a3ded38
Elad Shahar <SaladFork@Gmail.com>
2013-10-16 21:30:22 -0400

    Add game window

--- src/runner.py
+++ src/runner.py
@@ -1,8 +1,30 @@
+import sys
+import pygame
+from pygame.locals import *
+
 # Runs the main loop of the program
-class Runner:
+class Runner(object):
+
     def __init__(self, initial_screen):
+
+        pygame.init()
+
         self.screen = initial_screen
-    
+        self.main_display = pygame.display.set_mode((800, 600))
+
     def run(self):
-        while(True):
+
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if ((event.type == QUIT) or
+                    (event.type == KEYDOWN and event.key == K_ESCAPE)):
+                    running = False
+
             self.screen.update()
+            # TODO: Blit to main display
+            # self.main_display.update()
+
+        pygame.quit()
+        sys.exit()
