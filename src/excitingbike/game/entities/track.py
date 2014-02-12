45e1b7f7e25d493edef73a30adcacb51b28f4d30
Elad Shahar <SaladFork@Gmail.com>
2014-02-11 22:11:51 -0500

    Switch track to not be instantiated

--- src/excitingbike/game/entities/track.py
+++ src/excitingbike/game/entities/track.py
@@ -1,59 +1,62 @@
 import pygame
 
-class Track(object):
+track_sprite = None
 
-    def __init__(self):
-        self.track_sprite = pygame.image.load("assets/tracks.png").convert()
+track_hurdles = {
+    "START1": (  10,  64),
+    "START2": (  74,  64),
+    "START3": ( 139,  64),
+    "BLANK" : ( 202,  32),
+    "A"     : ( 234,  32),
+    "B"     : ( 266,  48),
+    "C"     : ( 314,  80),
+    "D"     : ( 394,  80),
+    "E"     : ( 474,  48),
+    "F"     : ( 522,  48),
+    "G"     : ( 570,  48),
+    "H"     : ( 618,  16),
+    "I"     : ( 634,  16),
+    "J"     : ( 650,  16),
+    "K"     : ( 666,  32),
+    "L"     : ( 698,  32),
+    "M"     : ( 730,  16),
+    "N"     : ( 746,  16),
+    "O"     : ( 762, 208),
+    "P"     : ( 970, 208),
+    "Q"     : (1178,  96),
+    "R"     : (1274, 208),
+    "S"     : (1482, 224),
+    "END"   : (1706,  96),
+}
 
-        self.track_hurdles = {
-            "START1": (  10,  64),
-            "START2": (  74,  64),
-            "START3": ( 139,  64),
-            "BLANK" : ( 202,  32),
-            "A"     : ( 234,  32),
-            "B"     : ( 266,  48),
-            "C"     : ( 314,  80),
-            "D"     : ( 394,  80),
-            "E"     : ( 474,  48),
-            "F"     : ( 522,  48),
-            "G"     : ( 570,  48),
-            "H"     : ( 618,  16),
-            "I"     : ( 634,  16),
-            "J"     : ( 650,  16),
-            "K"     : ( 666,  32),
-            "L"     : ( 698,  32),
-            "M"     : ( 730,  16),
-            "N"     : ( 746,  16),
-            "O"     : ( 762, 208),
-            "P"     : ( 970, 208),
-            "Q"     : (1178,  96),
-            "R"     : (1274, 208),
-            "S"     : (1482, 224),
-            "END"   : (1706,  96),
-        }
+TRACK_HEIGHT = 112
 
-        self.TRACK_HEIGHT = 112
+track_tints = [16 + TRACK_HEIGHT*i for i in range(8)]
 
-        self.track_tints = [16+self.TRACK_HEIGHT*i for i in range(8)]
-
-        self.tint = 0
+tint = 0
 
 
-    def getTrackHurdle(self, hurdleLetter):
-        return self.track_sprite.subsurface((
-            self.track_hurdles[hurdleLetter][0],
-            self.track_tints[self.tint],
-            self.track_hurdles[hurdleLetter][1],
-            self.TRACK_HEIGHT
-        ))
+def checkIfLoaded():
+    global track_sprite
+    if (track_sprite is None):
+        track_sprite = pygame.image.load("assets/tracks.png").convert()
 
-    def getThisTrack(self, track_list):
-        def add(x,y): return x+y
-        self.track_surface_array = [self.getTrackHurdle(hurdle_surface) for hurdle_surface in track_list]
-        self.track_width = sum((surface.get_width() for surface in self.track_surface_array))
-        self.track_surface = pygame.Surface((self.track_width,self.TRACK_HEIGHT))
-        self.width_counter = 0
-        for surface in self.track_surface_array:
-            self.track_surface.blit(surface, (self.width_counter,0))
-            self.width_counter += surface.get_width ()
-        return self.track_surface
+def getTrackHurdle(hurdleLetter):
+    checkIfLoaded()
+    return track_sprite.subsurface((
+        track_hurdles[hurdleLetter][0],
+        track_tints[tint],
+        track_hurdles[hurdleLetter][1],
+        TRACK_HEIGHT
+    ))
+
+def getThisTrack(track_list):
+    checkIfLoaded()
+    track_surface_array = [getTrackHurdle(hurdle_surface) for hurdle_surface in track_list]
+    track_width = sum((surface.get_width() for surface in track_surface_array))
+    track_surface = pygame.Surface((track_width,TRACK_HEIGHT))
+    width_counter = 0
+    for surface in track_surface_array:
+        track_surface.blit(surface, (width_counter,0))
+        width_counter += surface.get_width ()
+    return track_surface
--- src/excitingbike/screens/game_screen.py
+++ src/excitingbike/screens/game_screen.py
@@ -1,55 +1,43 @@
 import sys
 import pygame
+import excitingbike.game.entities.track as track
 from screen import Screen
-from ..game.entities.track import Track
 from pygame.locals import *
 from excitingbike.locals import *
 
 class GameScreen(Screen):
+
     def __init__(self):
         super(GameScreen,self).__init__()
         self.biker1xlocation = 0
         self.biker1currentacceleration = 0
         self.BGCOLOR = self.RED
 
-        self.current_track_list = ["START1","BLANK","BLANK","BLANK","A","G","BLANK","BLANK","B","BLANK","BLANK","BLANK","BLANK","BLANK","D","D","BLANK","BLANK","B","BLANK","BLANK","BLANK","BLANK","BLANK","D","D","BLANK","BLANK","B","BLANK","BLANK","END"]
-        self.current_track = Track()
-        self.current_track_surface = self.current_track.getThisTrack(self.current_track_list)
+        self.current_track_list = ["BLANK","BLANK","BLANK","A","G","BLANK","BLANK","B","BLANK","BLANK","BLANK","BLANK","BLANK","D","D","BLANK","BLANK","B","BLANK","BLANK","BLANK","BLANK","BLANK","D","D","BLANK","BLANK","B","BLANK","BLANK"]
+        self.current_track_surface = track.getThisTrack(self.current_track_list)
 
-    ## TODO Update Biker Location Modules
-    def place_holder_up(self):
-        print "Up Button Pressed"
+        self.bikerSpeed = 4
 
-    def place_holder_down(self):
-        print "Down Button Pressed"
+    def loadLevel(self, trackList):
 
-    def place_holder_left(self):
-        self.biker1xlocation += 10
-        print "Left Button Pressed"
+        self.track_surface_array = [self.getTrackHurdle(hurdle_surface) for hurdle_surface in track_list]
+        self.track_width = sum((surface.get_width() for surface in self.track_surface_array))
+        self.track_surface = pygame.Surface((self.track_width,self.TRACK_HEIGHT))
+        self.width_counter = 0
+        for surface in self.track_surface_array:
+            self.track_surface.blit(surface, (self.width_counter,0))
+            self.width_counter += surface.get_width ()
+        return self.track_surface
+        pass
 
-    def place_holder_right(self):
-        self.biker1xlocation -= 10
-        print "Right Button Pressed"
-
-    def place_holder_a(self):
-        print "A Button Pressed"
-
-    def place_holder_b(self):
-        print "B Button Pressed"
-
-    def place_holder_start(self):
-        print "Start Button Pressed"
-
-    def place_holder_select(self):
-        print "Select Button Pressed"
-
-    def update(self,events,states):
+    def update(self, events, states):
 
         self.currentOffset = -self.biker1xlocation + (10*self.biker1currentacceleration)
 
         # Event Operations
 
         for event in events:
+            """
             if event == KEY_DOWN:
                 self.place_holder_down()
             elif event == KEY_UP:
@@ -66,6 +54,8 @@ class GameScreen(Screen):
                 self.place_holder_start()
             elif event == KEY_SELECT:
                 self.place_holder_select()
+            """
+            pass
 
         # fill the screen with stuff to be updated
 
