99b8a9258cb2e66391a72ed30a86d57a9104b653
Elad Shahar <SaladFork@Gmail.com>
2014-03-03 21:59:53 -0500

    Add 3 second countdown with specific start states to game screen

--- src/excitingbike/screens/game_screen.py
+++ src/excitingbike/screens/game_screen.py
@@ -13,7 +13,12 @@ class GameScreen(Screen):
 
         self.temporary_track_list = ["BLANK","BLANK","BLANK","A","G","BLANK","BLANK","B","BLANK","BLANK","BLANK","BLANK","BLANK","D","D","BLANK","BLANK","B","BLANK","BLANK","BLANK","BLANK","BLANK","D","D","BLANK","BLANK","B","BLANK","BLANK"]
 
-        self.loadLevel(self.temporary_track_list)
+        self.start_hurdles = [track.getTrackHurdle(hurdle) for hurdle in ("START1", "START2", "START3")]
+        self.start_hurdle_width = self.start_hurdles[0].get_width()
+
+        self.track_surface = self.loadLevel(self.temporary_track_list)
+
+        self.resetGame()
 
         self.currentOffset = 0
         self.bikerSpeed = 4
@@ -23,17 +28,43 @@ class GameScreen(Screen):
         track_hurdles = [track.getTrackHurdle(hurdle) for hurdle in trackList]
         track_width = sum((surface.get_width() for surface in track_hurdles))
         track_height = track_hurdles[0].get_height()
-        self.track_surface = pygame.Surface((track_width, track_height))
+        track_surface = pygame.Surface((track_width, track_height))
 
         # For now, just blit the hurdles to the level once
-        xPos = 0
+        xPos = self.start_hurdle_width
         for surface in track_hurdles:
-            self.track_surface.blit(surface, (xPos, 0))
+            track_surface.blit(surface, (xPos, 0))
             xPos += surface.get_width()
+
+        return track_surface
+
+    def resetGame(self):
+        self.started = False
+        self.started_time = 0
 
     def update(self, events, states):
 
-        self.currentOffset -= self.bikerSpeed
+        self.displaysurf.blit(self.track_surface, (self.currentOffset, 0))
+
+        self.displaysurf.blit(self.start_hurdles[-1], (self.currentOffset, 0))
+
+        if not self.started:
+            if self.started_time == 0:
+                self.started_time = pygame.time.get_ticks()
+            else:
+                curTime = pygame.time.get_ticks()
+                msPassed = curTime - self.started_time
+
+                # TODO: make this agnostic as to the # of start hurdles
+                if msPassed >= 0 and msPassed < 1000:
+                    self.displaysurf.blit(self.start_hurdles[0], (0, 0))
+                elif msPassed >= 1000 and msPassed < 2000:
+                    self.displaysurf.blit(self.start_hurdles[1], (0, 0))
+                elif msPassed > 3000:
+                    self.started = True
+
+        else: # Game is active
+            self.currentOffset -= self.bikerSpeed
 
         # Event Operations
 
@@ -58,8 +89,3 @@ class GameScreen(Screen):
             """
             pass
 
-        # fill the screen with stuff to be updated
-
-        ### Place Holder Background
-
-        self.displaysurf.blit(self.track_surface, (self.currentOffset, 0))
