c29dcd29830c15ef79ae8657c33b3ebb030a6e49
PostRockFTW <jpviau@gmail.com>
2014-03-12 23:37:44 -0400

    Make track_hurdles and ordered dictionary and add Blank to level_builder_screen.hurdle_options

--- src/excitingbike/screens/level_builder_screen.py
+++ src/excitingbike/screens/level_builder_screen.py
@@ -6,7 +6,7 @@ from ..locals import *
 class LevelBuilderScreen(Screen):
     def __init__(self):
         super(LevelBuilderScreen,self).__init__()
-        self.location_index = 0
+        self.location_index = 10
         self.BGCOLOR = self.BLACK
 #Track objects
         self.current_track_list = ["START1","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","BLANK","END"]
@@ -20,13 +20,12 @@ class LevelBuilderScreen(Screen):
         self.options_surface_height = 56*self.SCREEN_MAGNIFIER
         self.options_surface = pygame.Surface((self.options_surface_width,self.options_surface_height))
         self.options_surface.fill(self.BGCOLOR)
-        self.not_hurdle_options = ["START1", "START2", "START3", "BLANK", "END"]
+        self.not_hurdle_options = ["START1", "START2", "START3", "END"]
         self.hurdle_options = Track.track_hurdles.keys()
-        self.hurdle_options.sort()
         for not_hurdle_option in self.not_hurdle_options:
             self.hurdle_options.remove(not_hurdle_option)
         self.hurdle_options_index = 0
-
+        self.highlighted_track = self.hurdle_options_index
     ## TODO Update Biker Location Modules
     def options_index_left(self):
         self.hurdle_options_index -= 1
@@ -68,7 +67,7 @@ class LevelBuilderScreen(Screen):
             elif event == KEY_RIGHT:
                 self.move_poistion_right()
             elif event == KEY_A_BUTTON:
-                self.place_holder_a()
+                print self.hurdle_options
             elif event == KEY_B_BUTTON:
                 self.place_holder_b()
             elif event == KEY_START:
@@ -76,7 +75,9 @@ class LevelBuilderScreen(Screen):
             elif event == KEY_SELECT:
                 self.place_holder_select()
 
-        # fill the screen with stuff to be updated
+        # Track State Update
+
+        self.current_track_surface = Track.getThisTrack(self.current_track_list)
 
         ### Place Holder Background
 
@@ -88,8 +89,11 @@ class LevelBuilderScreen(Screen):
 
         # Options Blit
         for i in range(len(self.hurdle_options)):
-                self.fontsurface = (self.myfont.render(self.hurdle_options[i], 1, self.WHITE))
-                self.displaysurf.blit(self.fontsurface, ((i*10*self.SCREEN_MAGNIFIER),200*self.SCREEN_MAGNIFIER))
+                if self.hurdle_options[i] == "Blank":
+                    self.fontsurface = (self.myfont.render(self.hurdle_options[i], 1, self.WHITE))
+                else:
+                    self.fontsurface = (self.myfont.render(self.hurdle_options[i], 1, self.WHITE))
+                    self.displaysurf.blit(self.fontsurface, ((i*10*self.SCREEN_MAGNIFIER),200*self.SCREEN_MAGNIFIER))
         self.fontsurface = (self.myfont.render("^", 1, self.WHITE))
         self.displaysurf.blit(self.fontsurface, ((self.hurdle_options_index*10*self.SCREEN_MAGNIFIER),210*self.SCREEN_MAGNIFIER))
 #        self.displaysurf.blit(self.surface, (x,y))
--- src/excitingbike/screens/option_menu_screen.py
+++ src/excitingbike/screens/option_menu_screen.py
@@ -7,6 +7,6 @@ class OptionMenuScreen(MenuScreen):
 
         self.set_menu_options(("INPUT OPTIONS", "AUDIO OPTIONS", "VIDEO OPTIONS"))
         self.menu_options_dictionary = {"INPUT OPTIONS":game_screen.GameScreen, "AUDIO OPTIONS":game_screen.GameScreen, "VIDEO OPTIONS":game_screen.GameScreen}
-
+        
 def update(self,events,states):
     super(OptionMenuScreen,self).update(events,states)
