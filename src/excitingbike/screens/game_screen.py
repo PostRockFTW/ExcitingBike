bda2e77b0b04f7d9d97387af6588affb42a34999
PostRockFTW <jpviau@gmail.com>
2014-12-11 21:43:04 -0500

    brake game_screen.update into method calls and define those methods

--- src/excitingbike/screens/game_screen.py
+++ src/excitingbike/screens/game_screen.py
@@ -15,6 +15,9 @@ class GameScreen(Screen):
 
         # Set up instances for gamescreen objects
 
+        #Initial state of the game
+        self.game_state = "started"
+
         #State Surface???
         self.background_surface =  pygame.image.load("assets/Excitebike_BackGround.png").convert()
 
@@ -35,15 +38,13 @@ class GameScreen(Screen):
                                                                                "START2",
                                                                                "START3")]
         self.start_hurdle_width = self.start_hurdles[0].get_width()
-        self.resetGame()
+        self.reset_game()
 
-    def resetGame(self):
+    def reset_game(self):
         self.started = False
         self.started_time = 0
 
-    def update(self, events, states):
-        #Update Background gfx to wipe screen
-
+    def blit_background(self):
         self.displaysurf.blit(self.background_surface, (0,0))
         self.displaysurf.blit(self.track.track_sprite,
                              (self.track.current_displaysurf_x_position,
@@ -52,28 +53,8 @@ class GameScreen(Screen):
                              (self.track.current_displaysurf_x_position,
                               self.track.displaysurf_y_position))
 
-        #Game start animation logic todo move game start animation to separate class
-        if not self.started:
-            if self.started_time == 0:
-                self.started_time = pygame.time.get_ticks()
-            else:
-                curTime = pygame.time.get_ticks()
-                msPassed = curTime - self.started_time
-
-                # TODO: make this agnostic as to the # of start hurdles
-                if 0 <= msPassed < 1000:
-                    self.displaysurf.blit(self.start_hurdles[0], (0, self.track.displaysurf_y_position))
-                elif 1000 <= msPassed < 2000:
-                    self.displaysurf.blit(self.start_hurdles[1], (0, self.track.displaysurf_y_position))
-                elif msPassed > 3000:
-                    self.started = True
-        #Rest of game logic
-        else: # Game is active
-            self.track.current_displaysurf_x_position -= self.biker_1.speed
-            self.biker_1.update_speed(self.biker_1.friction)
-            self.biker_1.update_current_lane()
-        # Handle inputs
-
+    def update_running_inputs(self,events):
+        Input.keys.update_keys(events)
         # FROM LOCALS
             #KEY_UP       = 0
             #KEY_DOWN     = 1
@@ -84,37 +65,61 @@ class GameScreen(Screen):
             #KEY_START    = 6
             #KEY_SELECT   = 7
             #KEY_ESCAPE   = 8
-
-        Input.keys.update_keys(events)
-
-        if Input.keys.down(0):
-            if self.lastEventStates[0] == False:
-                self.biker_1.update_target_lane(-1)
-        if Input.keys.down(1):
-            if self.lastEventStates[1] == False:
-                self.biker_1.update_target_lane(1)
-        if Input.keys.down(2):
-            self.biker_1.rotate_sprite_counter_clockwise()
-        if Input.keys.down(3):
-            self.biker_1.rotate_sprite_clockwise()
-        if Input.keys.down(4):
-            self.biker_1.update_speed(self.biker_1.acceleration_a)
-        if Input.keys.down(5):
-            self.biker_1.update_speed(self.biker_1.acceleration_b)
-        if Input.keys.down(6):
-            if self.heat > 0:
-                self.heat -= 1
-                pass
-        if Input.keys.down(7):
-            if self.heat < self.heatBarWidth:
-                self.heat += 1
-                pass
-        #if events[8]    == True:
-            #self.place_holder_escape()
-
+        # TODO add started, paused, and ending inputs
+        if self.game_state == "started":
+            pass
+        elif self.game_state == "paused":
+            pass
+        elif self.game_state == "running":
+            if Input.keys.down(0):
+                if self.lastEventStates[0] == False:
+                    self.biker_1.update_target_lane(-1)
+            if Input.keys.down(1):
+                if self.lastEventStates[1] == False:
+                    self.biker_1.update_target_lane(1)
+            if Input.keys.down(2):
+                self.biker_1.rotate_sprite_counter_clockwise()
+            if Input.keys.down(3):
+                self.biker_1.rotate_sprite_clockwise()
+            if Input.keys.down(4):
+                self.biker_1.update_speed(self.biker_1.acceleration_a)
+            if Input.keys.down(5):
+                self.biker_1.update_speed(self.biker_1.acceleration_b)
+            if Input.keys.down(6):
+                if self.heat > 0:
+                    self.heat -= 1
+                    pass
+            if Input.keys.down(7):
+                if self.heat < self.heatBarWidth:
+                    self.heat += 1
+                    pass
+            #if events[8]    == True:
+                #self.place_holder_escape()
+        elif self.game_state == "ending":
+            pass
         self.lastEventStates = events
 
-        # Update Foreground Graphics
+    def starting(self):
+        if self.started_time == 0:
+            self.started_time = pygame.time.get_ticks()
+        else:
+            curTime = pygame.time.get_ticks()
+            msPassed = curTime - self.started_time
+
+            # TODO: make this agnostic as to the # of start hurdles
+            if 0 <= msPassed < 1000:
+                self.displaysurf.blit(self.start_hurdles[0], (0, self.track.displaysurf_y_position))
+            elif 1000 <= msPassed < 2000:
+                self.displaysurf.blit(self.start_hurdles[1], (0, self.track.displaysurf_y_position))
+            elif msPassed > 3000:
+                self.game_state = "running"
+
+    def running(self,events):
+        self.track.current_displaysurf_x_position -= self.biker_1.speed
+        self.biker_1.update_speed(self.biker_1.friction)
+        self.biker_1.update_current_lane()
+
+    def blit_forground(self):
         #Biker
         self.displaysurf.blit(self.biker_1.sprite,
                               (0,
@@ -128,6 +133,18 @@ class GameScreen(Screen):
         pygame.draw.rect(self.displaysurf,
                          pygame.Color("red"),
                          heatBarRect)
+
+    def update(self, events, states):
+        #Update Background gfx to wipe screen
+        self.blit_background()
+        self.update_running_inputs(events)
+        if self.game_state == "started":
+            self.starting()
+        elif self.game_state == "running":
+            self.running(events)
+        elif self.game_state == "running":
+            self.running(events)
+        self.blit_forground()
 
     def yPosForLane(self, lane):
         laneHeight = 12
